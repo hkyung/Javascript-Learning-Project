f30e363355f7ef7b38ec731a68fcdceb270bcb17
hkyung <kaldente@gmail.com>
2015-01-29 08:49:43 -0800

    console and prompt added
    still testing...

---
+++ Javascript Learning Project/public_html/js/test1.js
@@ -0,0 +1,8 @@
+function printButtonText() {
+                document.getElementById("demo").innerHTML = "Paragraph changed.";
+                
+                confirm("this is confirm!");
+                
+                prompt("This is prompt!")
+            }
+
